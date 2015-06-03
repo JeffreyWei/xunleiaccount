2298a44ee1bec0dbc851a6b031197d73dd67ad8e
wei <mavenwei@qq.com>
2015-06-03 15:28:51 +0800

    edit app2

--- app2.py
+++ app2.py
@@ -21,28 +21,14 @@ def getXunLeiAccount():
         if(text.find("迅雷")>=0 and text.find("密码")>=0):
             print(line)
 
-def checkLink(title):
-    if title == None:
-        return -1
-    now = time.localtime(time.time())
-    # 会出现标题没有月份的主题
-    # date = str(now.tm_mon) + "月" + str(now.tm_mday) + "日"
-    date = str(now.tm_mday) + "日"
-    title = title.encode('utf-8')
-    return title.find(date);
-
 def getPage(url):
     '''下载文件html代码，找出一楼的核心代码'''
     opener = urllib2.build_opener()
     # 不加头信息则出现403错误和乱码
     opener.addheaders = [('User-agent', 'Mozilla/5.0')];
     htmlAll = opener.open(url).read()
-    # reg1Floor = '<div class="msgfont">(.*?)</div>'
-    # html = re.search(reg1Floor,htmlAll)
-    # html = html.group()
     # 文件保存编码和文件编辑编码都是utf-8，所以decode一次，不然会出现乱码，但是不影响结果。
     return htmlAll
-
 
 if __name__ == '__main__':
     getXunLeiAccount()
